fe24c8eecbb29e678c90ff270252bee5f938572d
Hugo <102437612+hugopino@users.noreply.github.com>
2026-01-22 08:32:03 -0300

    Refactor GitHub link constants for improved readability and maintainability

--- src/shared/constants/links.ts
+++ src/shared/constants/links.ts
@@ -1,7 +1,4 @@
-const GITHUB_LINK_URL = 'https://github.com/zenobank/crypto-logos';
-const GITHUB_GET_STARTED_LINK_URL = 'https://github.com/zenobank/crypto-logos?tab=readme-ov-file#-getting-started';
+const GITHUB_LINK_URL = "https://github.com/zenobank/crypto-logos";
+const GITHUB_GET_STARTED_LINK_URL = `${GITHUB_LINK_URL}?tab=readme-ov-file#-getting-started`;
 
-export {
-  GITHUB_LINK_URL,
-  GITHUB_GET_STARTED_LINK_URL,
-};
+export { GITHUB_LINK_URL, GITHUB_GET_STARTED_LINK_URL };
