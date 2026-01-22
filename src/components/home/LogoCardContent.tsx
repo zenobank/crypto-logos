bc5b42441df35978e582f5992d1ae596cf6d9b5f
Hugo <102437612+hugopino@users.noreply.github.com>
2026-01-22 10:49:09 -0300

    refactor: Update LogoCardContent styles and remove additional commented-out code for improved clarity

--- src/components/home/LogoCardContent.tsx
+++ src/components/home/LogoCardContent.tsx
@@ -1,14 +1,10 @@
 'use client';
 
 import { ReactNode, useState } from 'react';
-// import { BaselineIcon, Copy, Download, Link as LinkIcon, SparklesIcon } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 // components
 import { Image } from '@/components/ui/image';
-// import { Button } from '@/components/ui/button';
-// import CopyLogoPopover from '@/components/home/CopyLogoPopover';
-// import DownloadLogoDialog from '@/components/home/DownloadLogoDialog';
 
 // models
 import type LogoItemsResponse from '@/shared/models/logos/logo-items-response';
@@ -49,27 +45,16 @@ export default function LogoCardContent({
     return variant.light[0];
   }
 
-  // function handleOpenWebsite(): void {
-  //   if (logo.websiteLink) {
-  //     window.open(logo.websiteLink, '_blank', 'noopener,noreferrer');
-  //   }
-  // }
-
-  // function handleToggleWordmark(): void {
-  //   setShowWordmark(!showWordmark);
-  // }
-
   return (
     <>
       <Image
-        className="mx-auto mt-1.5 mb-4 h-10 min-w-10"
+        className="mx-auto mt-1.5 mb-4 h-16 min-w-22"
         fallbackText="No logo"
         src={currentLogo.url}
         alt={logo.name}
-        height={40}
-        width={showWordmark ? 240 : 40}
+        height={55}
+        width={showWordmark ? 240 : 55}
       />
-
       {children}
     </>
   );
