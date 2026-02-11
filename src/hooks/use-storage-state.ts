fe48b7cb3bc882efbfb8f8a2dbe9ce0f660d694a
Hugo <102437612+hugopino@users.noreply.github.com>
2026-02-11 12:41:15 -0300

    Refactor useStorageState hook to initialize state with initialState and load from storage in useEffect. This improves state management and ensures the latest stored value is reflected in the component state.

--- src/hooks/use-storage-state.ts
+++ src/hooks/use-storage-state.ts
@@ -3,9 +3,14 @@ import { useEffect, useState } from 'react';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function useStorageState<T=any>(initialState: T, key: string, storage: 'localStorage' | 'sessionStorage' = 'sessionStorage'): [T, (value: T) => void]  {
   // states
-  const [state, setState] = useState(window[storage].getItem(key) as T || initialState);
+  const [state, setState] = useState<T>(initialState);
 
   // effects
+  useEffect(() => {
+    const stored = window[storage].getItem(key);
+    if (stored) setState(stored as T);
+  }, [key, storage]);
+
   useEffect(() => {
     window[storage].setItem(key, JSON.stringify(state));
 
